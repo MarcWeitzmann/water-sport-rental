da88989199e6b5db7bbe94b7727f2a316ae492aa
Marc Weitzmann <marc@weitzmann.org>
2021-05-15 12:34:23 +0200

    updated the links, seed, categries

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,15 @@
 class CategoriesController < ApplicationController
 
+  before_action :set_school
+
   def index
     @categories = Category.all
+  end
+
+  private
+
+  def set_school
+    @school = School.find(params[:school_id])
   end
 
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,9 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show]
 
   def index
-    @items = Item.all.select{ |item| item.category_id == params["category_id"].to_i && item.school_id == params["school_id"].to_i }
+    # @items = Item.all.select{ |item| item.category_id == params["category_id"].to_i && item.school_id == params["school_id"].to_i }
+
+    @items = Item.where(category: params[:category_id], school: params[:school_id])
   end
 
   def show
