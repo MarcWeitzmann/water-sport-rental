f7b5a109e916137ee6b62807ab9300f5f07d73c1
Francisca Nunes <franciscaribeironunes@gmail.com>
2021-05-21 00:31:21 +0100

    Admin Schools

--- app/controllers/admin/schools_controller.rb
+++ app/controllers/admin/schools_controller.rb
@@ -15,10 +15,37 @@ module Admin
         def show
         end
 
+        def create
+            @school = School.new(school_params)
+            if @school.save
+                redirect_to admin_schools_path(@school), notice: "New School Saved"
+              else
+                render :new
+              end
+        end
+
+        def edit
+            @school = School.find(params[:id])
+        end
+
+        def update
+            @school = School.find(params[:id])
+            if @school.update_attributes(school_params)
+              flash[:success] = "School updated!"
+              redirect_to admin_schools_path(@school)
+            else
+              render action: :edit
+            end
+          end
+
         private
 
         def set_school
             @school = School.find(params[:id])
         end
+
+        def school_params
+            params.require(:school).permit(:name, :street, :city, :email,:phone, :user_id, :photo)
+          end
     end
 end
