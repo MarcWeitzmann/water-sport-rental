9244d67cd9189305687a942174fa2a783ae4df05
Francisca Nunes <franciscaribeironunes@gmail.com>
2021-05-22 12:48:10 +0100

    Admin Schools Add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,13 @@ class PagesController < ApplicationController
   def home
   end
 
+  private
+
+   def is_admin?
+     unless current_user.is_admin?
+       flash.alert = "Sorry, you don't have permissions to perform this action."
+       redirect_to root_path
+     end
+   end
+
 end
